311479d57c43b24f1e71636d9a79f3751d86a950
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-06-16 15:02:03 +0500

    "api url harcode"

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://connecthearpolling.vercel.app/api/auth'; 
+const API_URL =  'http://connecthearpolling.vercel.app/api/auth'; 
 
 const authService = {
   login: async (email, password) => {
